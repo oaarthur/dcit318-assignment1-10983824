9928cd2a862eaf1f50c96d2dc4d4f3233a692ba8
ember_power <oaarthur002@st.ug.edu.gh>
2024-06-02 16:19:44 +0100

    first commit

---
+++ gradeCalc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter the Marks ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (a >= 80 && a <= 89)
+            {
+                Console.WriteLine("B");
+            }
+            else if (a >= 70 && a <= 79)
+            {
+                Console.WriteLine("C");
+            }
+            else if (a >= 60 && a <= 69)
+            {
+                Console.WriteLine("D");
+            }
+            else if (a < 60)
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
