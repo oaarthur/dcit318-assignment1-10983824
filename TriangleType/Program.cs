91c9db33129b7cafe3b7e48b26c44a3c724c1fbd
Carlton Akussah <carlton@Carltons-MacBook-Pro.local>
2025-08-04 22:30:49 +0000

    first commit

---
+++ TriangleType/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Triangle
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            Console.WriteLine("Enter Length Of The Base ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Length Of First Side ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Length of Second Side ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if(c == b && c == a){
+                Console.WriteLine("This Is An Equilateral Triangle. All the sides are equal");
+            }
+            else if(a == b && c != b && c != a){
+                Console.WriteLine("This Is An Issoceles Triangle. Only two sides are equal");
+            }
+            else if(c == b && a != b && a != c){
+                Console.WriteLine("This Is An Issoceles Triangle. Only two sides are equal");
+            }
+            else if(a == c && b != c && b != a){
+                Console.WriteLine("This Is An Issoceles Triangle. Only two sides are equal");
+            }
+            else{
+                Console.WriteLine("This Is A Scalene Triangle. All the sides are not equal");
+            }
+        }
+    }
+}
